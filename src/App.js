e3df25cd7f91e7937fba2d19bd4046cf32603b89
M1kk4 <sven.korosec@gmail.com>
2022-02-20 20:33:32 +0100

    added home.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
+
 
 function App() {
   return (
     <div className="App">
       
       <Navbar/>
+      <Home />
        
     </div>
   );
---
+++ src/home.js
@@ -0,0 +1,12 @@
+const home = () => {
+    return (
+        <div className="home">
+            <h1>Home Page</h1>
+
+        </div>
+
+
+      );
+}
+ 
+export default home;
